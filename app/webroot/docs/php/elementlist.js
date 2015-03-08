87ae7df2fac044a2bd7d7148e1ebc4ca6e2e055f
mantir <mkapp2@gmx.de>
2015-03-08 15:47:44 +0100

    .gitignore is now working

--- app/webroot/docs/php/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","AppController"],["c","AppModel"],["c","console"],["c","Export"],["c","ExportsController"],["c","f"],["c","Input"],["c","InputsController"],["c","Log"],["c","LogsController"],["c","Measure"],["c","Method"],["c","MethodsController"],["c","PagesController"],["c","Project"],["c","ProjectsController"],["c","s"],["c","Unit"],["c","UnitsController"],["c","User"],["c","UsersController"],["c","Value"],["c","ValuesController"]];
